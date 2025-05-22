d2cfc60fce1f27ed173234fa122031fc863a6f87
KevinSoaresFC <soareskevin744@gmail.com>
2025-05-22 16:38:07 -0300

    scripts-do-projeto

---
+++ scripts.js
@@ -0,0 +1,14 @@
+let imagem = document.querySelector(".copo_grande_1")
+let circulo = document.querySelector(".circulo")
+
+function trocaimagem(endereco) {
+    imagem.src = endereco
+}
+
+function trocacor(cor) {
+    circulo.style.background = cor
+}
+
+
+    
+
